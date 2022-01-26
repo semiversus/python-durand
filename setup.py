b16c74b73154c8f599993d8337964c42c6f6d7f7
Günther Jena <guenther@jena.at>
2022-01-26 10:03:31 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,37 @@
 #!/usr/bin/env python
-import setuptools
+# -*- coding: utf-8 -*-
 
-if __name__ == "__main__":
-    setuptools.setup(use_scm_version=True)
+""" The setup script."""
+
+from setuptools import find_packages, setup
+
+
+with open('README.rst', 'rb') as readme_file:
+    readme = readme_file.read().decode('utf-8')
+
+
+setup(
+    name='durand',
+    use_scm_version={"write_to": "durand/_version.py"},
+    description='CANOpen library providing functionality to implement nodes',
+    long_description=readme,
+    url='https://github.com/semiversus/python-durand',
+    author='Günther Jena',
+    author_email='guenther@jena.at',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.7',
+    license='MIT license',
+    keywords='canopen can node ds301',
+    packages=find_packages(include=['durand*']),
+    zip_safe=False,
+)
