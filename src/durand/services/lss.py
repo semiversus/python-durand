9c6d1a1c61da18462d9cc3d5743fdc3614e11eb2
Günther Jena <guenther@jena.at>
2022-04-26 11:31:22 +0200

    lss working

--- src/durand/services/lss.py
+++ src/durand/services/lss.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING
 from enum import IntEnum
 import logging
+import struct
 
 from .nmt import StateEnum
 from durand.scheduler import get_scheduler
@@ -22,7 +23,9 @@ class LSS:
 
         self._state = LSSState.WAITING
 
-        self._received_address = [None] * 4
+        self._received_selective_address = [None] * 4
+        self._remote_slave_address = [None] * 6
+        self._fastscan_state = 0
 
         self._pending_baudrate = None
         self._change_baudrate_cb = None
@@ -36,7 +39,7 @@ class LSS:
     def on_nmt_state_update(self, state: StateEnum):
         if state == StateEnum.INITIALISATION:
             self._state = LSSState.WAITING
-            self._received_address = [None] * 4
+            self._received_selective_address = [None] * 4
 
     def _get_own_address(self):
         lss_address = [None] * 4
@@ -51,67 +54,173 @@ class LSS:
         cs = msg[0]
 
         if self._state == LSSState.WAITING:
-            if cs not in (0x04, 0x40, 0x41, 0x42, 0x43):
-                return
-        elif cs not in (0x04, 0x11, 0x13, 0x15, 0x17, 0x5a, 0x5b, 0x5c, 0x5d, 0x5e):
-                return
+            method = _waiting_cs_dict.get(cs, None)
+        else:
+            method = _configuration_cs_dict.get(cs, None)
 
-        if cs == 0x04:  # switch state global
-            mode = msg[1]
-            if mode not in (0, 1):
-                return
+        if method is None:
+            return
+        
+        method(self, msg)
+        
+    def cmd_switch_mode_global_configuration(self, msg: bytes):
+        if msg[1] != 1:  # check if requested mode is CONFIGURATION
+            return
 
-            if self._state == mode:
-                return
+        self._state = LSSState.CONFIGURATION
 
-            if mode == LSSState.WAITING and self._node.node_id == 0xFF and self._node.nmt.pending_node_id != 0xFF:
-                self._node.nmt.reset()
+    def cmd_switch_mode_global_waiting(self, msg: bytes):
+        if msg[1] != 0:  # check if requested mode is WAITING
+            return
+    
+        if self._node.node_id == 0xFF and self._node.nmt.pending_node_id != 0xFF:
+            self._node.nmt.reset()
 
-            self._state = mode
-        elif cs in (0x40, 0x41, 0x42, 0x43):  # select vendor id, product code, revision number, serial number
-            self._received_address[cs - 0x40] = int.from_bytes(msg[1:5], 'little', signed=False)
+        self._state = LSSState.WAITING
 
-            if cs != 0x43:
-                # TODO: should it only check on serial number or as soon as it matches?
-                return
+    def cmd_switch_mode_selective(self, msg: bytes):
+        index = msg[0] - 0x40
 
-            if (None in self._received_address):
-                return
+        self._received_selective_address[index] = int.from_bytes(msg[1:5], 'little', signed=False)
 
-            if self._received_address == self._get_own_address():
-                self._state = LSSState.CONFIGURATION
-                self._node.adapter.send(0x7e4, b"\x44" + bytes(7))
-        elif cs in (0x5a, 0x5b, 0x5c, 0x5d):  # inquire vendor id, product code, revision number, serial number
-            value = self._get_own_address()[cs - 0x5a]
-            self._node.adapter.send(0x7e4, msg[:1] + value.to_bytes(4, 'little') + bytes(3))
-        elif cs == 0x5e:  # inquire node id
-            self._node.adapter.send(0x7e4, b"\x5e" + self._node.node_id.to_bytes(1, 'little') + bytes(6))
-        elif cs == 0x11:  # set node id
-            node_id = msg[1]
+        if None in self._received_selective_address:
+            return
 
-            if 1 <= node_id <= 127 or node_id == 0xff:
-                self._node.nmt.set_pending_node_id(msg[1])
-                result = 0
-            else:
-                result = 1
+        if self._received_selective_address == self._get_own_address():
+            self._state = LSSState.CONFIGURATION
+            self._node.adapter.send(0x7e4, b"\x44" + bytes(7))
+        
+        self._received_selective_address == [None] * 4
+    
+    def cmd_inquire_identity(self, msg: bytes):
+        index = msg[0] - 0x5a
+        value = self._get_own_address()[index]
+        self._node.adapter.send(0x7e4, msg[:1] + value.to_bytes(4, 'little') + bytes(3))
+    
+    def cmd_inquire_node_id(self, msg: bytes):
+        self._node.adapter.send(0x7e4, b"\x5e" + self._node.node_id.to_bytes(1, 'little') + bytes(6))
 
-            self._node.adapter.send(0x7e4, b"\x11" + result.to_bytes(1, 'little') + bytes(6))
-        elif cs == 0x13:  # configure bit timing
-            if msg[1] != 0 or msg[2] in (0, 1, 2, 3, 4, 6, 7, 8) or self._change_baudrate_cb is None:
-                self._node.adapter.send(0x7e4, b"\x13\x01" + bytes(6))
-                return
+    def cmd_configure_node_id(self, msg: bytes):
+        node_id = msg[1]
 
-            self._pending_baudrate = msg[2]
-            self._node.adapter.send(0x7e4, b"\x13\x00" + bytes(6))
-        elif cs == 0x15:  # activate bit timing
-            delay = int.from_bytes(msg[1:3], 'little') / 1000  # [seconds]
-            if self._pending_baudrate is not None:
-                get_scheduler().add(delay, self._change_baudrate, args=(delay,))
-        elif cs == 0x17:  # store configuration
-            # store configuration is not supported
-            self._node.adapter.send(0x7e4, b"\x17\x01" + bytes(6))
+        if 1 <= node_id <= 127 or node_id == 0xff:
+            self._node.nmt.pending_node_id = node_id
+            result = 0
+        else:
+            result = 1
 
+        self._node.adapter.send(0x7e4, b"\x11" + result.to_bytes(1, 'little') + bytes(6))
+
+    def cmd_configure_bit_timing(self, msg: bytes):
+        valid_table_entries = (0, 1, 2, 3, 4, 6, 7, 8)
+        selector, index = msg[1:3]
+
+        if selector != 0 or index not in valid_table_entries or self._change_baudrate_cb is None:
+            self._node.adapter.send(0x7e4, b"\x13\x01" + bytes(6))
+            return
+
+        self._pending_baudrate = index
+        self._node.adapter.send(0x7e4, b"\x13\x00" + bytes(6))
+
+    def cmd_activate_bit_timing(self, msg: bytes):
+        delay = int.from_bytes(msg[1:3], 'little') / 1000  # [seconds]
+        
+        if self._pending_baudrate is not None:
+            get_scheduler().add(delay, self._change_baudrate, args=(delay,))
+    
     def _change_baudrate(self, delay: float):
         self._change_baudrate_cb(self._pending_baudrate)
         self._pending_baudrate = None
         get_scheduler().add(delay, self._node.nmt.reset)
+    
+    def cmd_store_configuration(self, msg: bytes):
+        # store configuration is not supported
+        self._node.adapter.send(0x7e4, b"\x17\x01" + bytes(6))
+
+    def cmd_identify_remote_slaves(self, msg: bytes):
+        index = msg[0] - 0x46
+        value = int.from_bytes(msg[1:5], 'little')
+        self._remote_slave_address[index] = value
+
+        if None in self._remote_slave_address:
+            return
+        
+        vendor, product, revision, serial = self._get_own_address()
+        
+        if (vendor == self._remote_slave_address[0] and
+                product == self._remote_slave_address[1] and
+                self._remote_slave_address[2] <= revision <= self._remote_slave_address[3] and
+                self._remote_slave_address[4] <= serial <= self._remote_slave_address[5]):
+
+            self._node.adapter.send(0x7e4, b"\x47" + bytes(7))
+
+        self._remote_slave_address = [None] * 6
+    
+    def cmd_identify_nonconfigured_remote_slaves(self, msg: bytes):
+        if self._node.node_id == 0xFF:
+            self._node.adapter.send(0x7e4, b"\x50" + bytes(7))
+
+    def cmd_fastscan(self, msg: bytes):
+        if self._node.node_id != 0xFF:
+            return
+        
+        id_number, bit_checked, lss_sub, lss_next = struct.unpack('IBBB', msg[1:])
+
+        if bit_checked == 0x80:
+            self._fastscan_state = 0
+            self._node.adapter.send(0x7e4, b"\x4F" + bytes(7))
+            return
+        
+        if lss_sub != self._fastscan_state:
+            return
+
+        mask = ~((1 << bit_checked) - 1)
+        lss_address = self._get_own_address()
+        
+        if (lss_address[lss_sub] & mask) != (id_number & mask):
+            return
+        
+        self._fastscan_state = lss_next
+
+        if bit_checked == 0 and lss_sub == 3:
+            self._state = LSSState.CONFIGURATION
+        
+        self._node.adapter.send(0x7e4, b"\x4F" + bytes(7))
+
+
+_waiting_cs_dict = {
+    0x04: LSS.cmd_switch_mode_global_configuration,
+    0x40: LSS.cmd_switch_mode_selective,
+    0x41: LSS.cmd_switch_mode_selective,
+    0x42: LSS.cmd_switch_mode_selective,
+    0x43: LSS.cmd_switch_mode_selective,
+    0x46: LSS.cmd_identify_remote_slaves,
+    0x47: LSS.cmd_identify_remote_slaves,
+    0x48: LSS.cmd_identify_remote_slaves,   
+    0x49: LSS.cmd_identify_remote_slaves,
+    0x4a: LSS.cmd_identify_remote_slaves,
+    0x4b: LSS.cmd_identify_remote_slaves,
+    0x4c: LSS.cmd_identify_nonconfigured_remote_slaves,
+    0x51: LSS.cmd_fastscan,
+}
+
+_configuration_cs_dict = {
+    0x04: LSS.cmd_switch_mode_global_waiting,
+    0x11: LSS.cmd_configure_node_id,
+    0x13: LSS.cmd_configure_bit_timing,
+    0x15: LSS.cmd_activate_bit_timing,
+    0x17: LSS.cmd_store_configuration,
+    0x5a: LSS.cmd_inquire_identity,
+    0x5b: LSS.cmd_inquire_identity,
+    0x5c: LSS.cmd_inquire_identity,
+    0x5d: LSS.cmd_inquire_identity,
+    0x5e: LSS.cmd_inquire_node_id,
+    0x46: LSS.cmd_identify_remote_slaves,
+    0x47: LSS.cmd_identify_remote_slaves,
+    0x48: LSS.cmd_identify_remote_slaves,
+    0x49: LSS.cmd_identify_remote_slaves,
+    0x4a: LSS.cmd_identify_remote_slaves,
+    0x4b: LSS.cmd_identify_remote_slaves,
+    0x4c: LSS.cmd_identify_nonconfigured_remote_slaves,
+    0x51: LSS.cmd_fastscan,
+}
